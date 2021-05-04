fa1cdd525ec05fd7a7764ab033f8bd162468743d
Pasilici <82535837+Pasilici@users.noreply.github.com>
2021-05-04 17:12:46 +0300

    Add files via upload

--- imaginiproiect.py
+++ imaginiproiect.py
@@ -10,16 +10,15 @@ page = requests.get(BASE_URL, headers = my_headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
+
 #print(soup.prettify())
 
-#labs = re.findall(r'((?<=<h3>)(.+?)(?=<\/h3>))', soup.prettify())
+#labs = re.findall(r'((?<=<a+img src>)(.+?)(?=<\/a+img src>))', soup.prettify())
+headings = soup.find_all("img src")
 
-headings = soup.find_all('h3')
-
-f=open("labs.txt", "w")
+f = open("lab.txt", "w")
 
 for heading in headings:
     f.write(heading.text)
 
 f.close()
-
